7f0cf07cdefe243ca970d14a397d990ceea81fb4
ovseychuk <ovseychuk@teleofis.ru>
2016-08-05 09:52:50 +0300

    pollmydevice beta

--- feeds/luci/applications/luci-app-pollmydevice/luasrc/controller/pollmydevice.lua
+++ feeds/luci/applications/luci-app-pollmydevice/luasrc/controller/pollmydevice.lua
@@ -1,14 +1,14 @@
-
 module("luci.controller.pollmydevice", package.seeall)
 
 function index()
 
     if not nixio.fs.access("/etc/config/pollmydevice") then
-            return
+        return
     end
 
     local uci = require("luci.model.uci").cursor()
-    local page
+    local page 
+    local nameifc = '0'
     page = node("admin", "services")
 
     page = entry({"admin", "services", "pollmydevice"}, cbi("pollmydevice"), _(translate("PollMyDevice")))
@@ -16,10 +16,14 @@ function index()
     page.subindex = true
 
     uci:foreach("pollmydevice", "interface",
-            function (section)
-                    local ifc = section[".name"]
-                    entry({"admin", "services", "pollmydevice", ifc},
-                    true, ifc:upper())
-            end)
+        function (section)
+            local ifc = section[".name"]
+            if ifc == '0' then
+                nameifc = 'RS485'
+            elseif ifc == '1' then
+                nameifc = 'RS232'
+            end
+            entry({"admin", "services", "pollmydevice", ifc}, true, nameifc:upper())
+        end)
 
 end
--- feeds/luci/applications/luci-app-pollmydevice/luasrc/model/cbi/pollmydevice.lua
+++ feeds/luci/applications/luci-app-pollmydevice/luasrc/model/cbi/pollmydevice.lua
@@ -5,12 +5,12 @@ require 'luci.sys'
 
 arg[1] = arg[1] or ""
 
-local has_iface = nixio.fs.access("/etc/config/pollmydevice")
-
-m = Map("pollmydevice", translate("PollMyDevice") .. " " .. arg[1]:upper(), translate("TCP to RS232/RS485 converter"))
+m = Map("pollmydevice", translate("PollMyDevice"), translate("TCP to RS232/RS485 converter"))
 
 s = m:section(NamedSection, arg[1], "pollmydevice", translate("Utility Settings"))
 s.addremove = false
+
+teleofisid = s:option(DummyValue, "teleofisid",  translate("Teleofis ID"))
 
 mode = s:option(ListValue, "mode", translate("Mode"))
   mode.default = "disabled"
@@ -45,16 +45,21 @@ parity = s:option(ListValue, "parity", translate("Parity"))
 stopbits = s:option(ListValue, "stopbits", translate("StopBits"))
   stopbits.default = 1
   stopbits:value(1)
-  stopbits:value(1.5)
   stopbits:value(2)
   stopbits.optional = false
-  stopbits.datatype = "float"
+  stopbits.datatype = "uinteger"
 
 server_port = s:option(Value, "server_port",  translate("Server Port"))
   server_port.default = 33333
   server_port.datatype = "and(uinteger, min(1025), max(65535))"
   server_port.rmempty = false
   server_port.optional = false
+
+conn_time = s:option(Value, "conn_time",  translate("Connection Hold Time"))
+  conn_time.default = 60
+  conn_time.datatype = "and(uinteger, min(0), max(100000))"
+  conn_time.rmempty = false
+  conn_time.optional = false
 
 client_host = s:option(Value, "client_host",  translate("Client Host or IP Address"))
   client_host.datatype = "string"
@@ -64,6 +69,12 @@ client_port = s:option(Value, "client_port",  translate("Client Port"))
   client_port.datatype = "and(uinteger, min(1025), max(65535))"
   client_port.rmempty = false
   client_port.optional = false
+
+client_timeout = s:option(Value, "client_timeout",  translate("Client Reconnection Timeout"))
+  client_timeout.default = 60
+  client_timeout.datatype = "and(uinteger, min(0), max(100000))"
+  client_timeout.rmempty = false
+  client_timeout.optional = false
 
 client_auth = s:option(Flag, "client_auth", translate("Client Authentification"), translate("Use Teleofis Authentification"))  -- create enable checkbox
   client_auth.rmempty = false
