f5eaab1561374b4db2ed8d6642221fbe4ce54e8c
iuncuim <iuncuim@gmail.com>
2017-05-03 12:14:16 +0300

    TELEOFIS: branch

--- doresult.sh
+++ doresult.sh
@@ -1,7 +1,7 @@
 mkdir result
 sudo cp rootfs.img ./result/rootfs.img
 sudo cp ./bin/mxs/openwrt-mxs-uImage ./result/openwrt-mxs-uImage
-sudo cp ./build_dir/target-arm_arm926ej-s_uClibc-0.9.33.2_eabi/linux-mxs/linux-3.18.29/arch/arm/boot/dts/imx28-duckbill.dtb ./result/fdt.dtb
+sudo cp ./build_dir/target-arm_arm926ej-s_uClibc-0.9.33.2_eabi/linux-mxs/linux-3.18.29/arch/arm/boot/dts/imx28-teleofis.dtb ./result/fdt.dtb
 cd result
 md5sum fdt.dtb > check.md5
 md5sum openwrt-mxs-uImage >> check.md5
