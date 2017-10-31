68134fbe5648c36e2c8fd75652f898844fdbb5c1
iuncuim <iuncuim@gmail.com>
2017-10-31 17:44:26 +0300

    LUCI-APP-IOLINES: added

---
+++ feeds/luci/applications/luci-app-iolines/luasrc/model/cbi/iolines.lua
@@ -0,0 +1,41 @@
+local fs  = require "nixio.fs"
+local sys = require "luci.sys"
+
+m = Map("iolines", "Universal I/O lines", translate("Mode 1 - voltage measurement; Mode 2 - connection of resistance sensors; Mode 3 - load management"))
+
+s = m:section(TypedSection, "io")
+s.anonymous = true
+s.addremove = false
+s.template = "cbi/tblsection"
+
+o = s:option(DummyValue, "name", translate("I/O name"))
+
+mode = s:option(ListValue, "mode", translate("Mode"))
+  mode.default = "mode1"
+  mode:value("mode1", "Mode 1")
+  mode:value("mode2", "Mode 2")
+  mode:value("mode3", "Mode 3")
+  mode.optional = false
+
+enabled = s:option(Flag, "enabled", translate("Run on startup"))
+  enabled.rmempty = false
+
+voltage = s:option(DummyValue, "voltage", translate("Measured voltage on ADC, mV"))
+    function voltage.cfgvalue(self, section)
+        local dev = self.map:get(section, "dev")
+        local test = io.popen("cat %s" %{dev})
+        local result = test:read("*a")
+        test:close()
+        result = result * 4.365
+        return result
+    end
+
+refresher = s:option( Button, "refresher", translate("Refresh measuring") )  
+  refresher.title      = translate("Refresh measuring")
+  refresher.inputtitle = translate("Refresh")
+  refresher.inputstyle = "apply"
+  function refresher.write()
+     
+  end
+
+return m
