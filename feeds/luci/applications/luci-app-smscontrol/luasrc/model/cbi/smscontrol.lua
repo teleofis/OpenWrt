e4e7b3696fc03353a0ccb50f8cef0ededa37c119
iuncuim <iuncuim@gmail.com>
2018-01-26 16:30:20 +0300

    SMSCONTROL: translate

--- feeds/luci/applications/luci-app-smscontrol/luasrc/model/cbi/smscontrol.lua
+++ feeds/luci/applications/luci-app-smscontrol/luasrc/model/cbi/smscontrol.lua
@@ -1,7 +1,7 @@
 local fs  = require "nixio.fs"
 local sys = require "luci.sys"
 
-m = Map("smscontrol", "Remote SMS control", translate("For example, if you enter '1234' for the password and send an SMS with the content of '1234;reboot' to the router, the router will reboot."))
+m = Map("smscontrol", "Remote SMS control", translate("Here you can send commands to router via a call or SMS"))
 
 n = m:section(NamedSection, "common", "smscontrol")
 
@@ -24,12 +24,12 @@ pass = n:option(Value, "pass",  translate("Password"))
   pass.optional = false
   pass.datatype = "and(uciname,maxlength(15))"
 
-whitelist = n:option(DynamicList, "whitelist",  translate("Allowed numbers"))
+whitelist = n:option(DynamicList, "whitelist",  translate("Allowed phone numbers"))
   whitelist.datatype = "phonedigit"
   whitelist.cast = "string"
   whitelist.rmempty =true
 
-s = m:section(TypedSection, "remote", translate("Command over SMS"))
+s = m:section(TypedSection, "remote", translate("Command over SMS"), translate("For example, if you enter '1234' as a password and send an SMS command '1234;reboot' to the router, the router will reboot"))
 s.rmempty = true
 s.anonymous = true
 s.addremove = true
@@ -49,7 +49,29 @@ command = s:option(Value, "command", translate("Linux command"))
   command.rmempty =false
   command.optional = false
 
-c = m:section(TypedSection, "call", translate("Command over call"))
+l = m:section(TypedSection, "cli", translate("Universal command over SMS"), translate("For example, if you enter '1234' as a password and send an SMS command '1234;CLI$ifconfig br-lan down' to the router, the router will execute CLI-command ifconfig br-lan down"))
+l.rmempty = true
+l.anonymous = true
+l.addremove = false
+l.template = "cbi/tblsection"
+
+enabled = l:option(Flag, "enabled", translate("Enable"))
+  enabled.rmempty = false
+
+ack = l:option(Flag, "ack", translate("Reply via SMS"))
+  ack.rmempty = true
+
+received = l:option(DummyValue, "received", translate("Message text"))
+  received.rmempty =false
+  received.optional = false
+  received.value = "CLI$any_cli_command_here"
+
+command = l:option(DummyValue, "command", translate("Linux command"))
+  command.rmempty =false
+  command.optional = false
+  command.value = "any_cli_command_here"
+
+c = m:section(TypedSection, "call", translate("Command over call"), translate("You can make a call from the allowed phone number and then the router will execute the command"))
 c.rmempty = true
 c.anonymous = true
 c.addremove = false
@@ -68,7 +90,7 @@ command = c:option(Value, "command", translate("Linux command"))
 k = m:section(NamedSection, "send", "smscontrol", translate("Send SMS"))
 k.rmempty = true
 
-to = k:option(Value, "to",  translate("To number"))
+to = k:option(Value, "to",  translate("To phone number"))
   to.datatype = "phonedigit"
   to.cast = "string"
   to.rmempty =true
